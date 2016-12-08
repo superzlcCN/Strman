8307e15aadc6dc639154926420b4dc4e5e0ac1b8
zlc <307170396@qq.com>
2016-12-08 22:46:15 +0800

    2016-12-8

--- src/main/java/BUPT/ZLC/Strman/Strman.java
+++ src/main/java/BUPT/ZLC/Strman/Strman.java
@@ -1,5 +1,7 @@
 package BUPT.ZLC.Strman;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Optional;
 import java.util.StringJoiner;
 import java.util.function.Predicate;
@@ -72,5 +74,201 @@ public abstract class Strman {
 		return (index >= 0 && index < length)?Optional.of(String.valueOf(value.charAt(index))):Optional.empty();
 	}
 
+	public static String[] between(final String value,final String start, final String end){
+		validate(value, NULL_STRING_PREDICATE, NULL_STRING_MSG_SUPPLIER);
+		validate(start, NULL_STRING_PREDICATE, NULL_STRING_MSG_SUPPLIER);
+		validate(end, NULL_STRING_PREDICATE, NULL_STRING_MSG_SUPPLIER);
+
+		String[] parts = value.split(end);
+		
+		return Arrays.stream(parts)
+				.map(str -> str.substring( value.indexOf(start) + start.length())).toArray(String[]::new);
+	}
+	/**
+	 * Return a String array consisting of the characters in the String.
+	 * @param value		input
+	 * @return	character array
+	 */
+	public static String[] chars(final String value){
+		validate(value, NULL_STRING_PREDICATE, NULL_STRING_MSG_SUPPLIER);
+		return value.split("");
+	}
 	
+	/**
+	 * Replace consecutive whitespace characters with a single space
+	 * @param value
+	 * @return
+	 */
+	public static String collapseWhitespace(final String value){
+		validate(value, NULL_STRING_PREDICATE, NULL_STRING_MSG_SUPPLIER);
+		return value.trim().replaceAll("\\s\\s+", " ");
+	}
+	
+	public static boolean contains(final String value,final String needle){
+		return contains(value, needle,false);
+	}
+	
+	public static boolean contains(final String value, final String needle, final boolean isCaseSensitive){
+		validate(value, NULL_STRING_PREDICATE, NULL_STRING_MSG_SUPPLIER);
+		if (isCaseSensitive) {
+			return value.contains(needle);
+		}
+		
+		return value.toUpperCase().contains(needle.toUpperCase());
+	}
+	/**
+	 * Verify that all needles are contained in value.The search is case insentive
+	 * @param value			input String to search
+	 * @param needles		needles to find
+	 * @return true			if all needles are found else false.
+	 */
+	public static boolean containsAll(final String value,final String[] needles) {
+		validate(value, NULL_STRING_PREDICATE, NULL_STRING_MSG_SUPPLIER);
+		return Arrays.stream(needles).allMatch(needle -> contains(value, needle,true));
+	}
+	/**
+	 * Verifies that all needles are contained in value
+	 * @param value				input String to search
+	 * @param needles			needles to find
+	 * @param caseSensivie		true or false
+	 * @return	if all needles are found else false.
+	 */
+	public static boolean containsAll(final String value, final String[] needles,final boolean caseSensivie){
+		validate(value, NULL_STRING_PREDICATE, NULL_STRING_MSG_SUPPLIER);
+		return Arrays.stream(needles).allMatch(needle -> contains(value, needle, caseSensivie));
+	}
+	
+	/**
+	 * Verifies that one or more of needles are contained in value.
+	 * @param value					input
+	 * @param needle				needles to search
+	 * @return	boolean true if any needle is found else false.
+	 */
+	public static boolean containsAny(final String value, final String[] needle){
+		return containsAny(value, needle,false);
+	}
+	
+	/**
+	 * Verifies that one or more of needles are contained in value.
+	 * @param value				input
+	 * @param needles			needles to search
+	 * @param caseSensitive		true or false
+	 * @return	boolean true if any needle is found else false
+	 */
+	public static boolean containsAny(final String value, final String[] needles,final boolean caseSensitive) {
+		validate(value, NULL_STRING_PREDICATE, NULL_STRING_MSG_SUPPLIER);
+		return Arrays.stream(needles).anyMatch(needle -> contains(value, needle, caseSensitive));
+	}
+	/**
+	 * Count the number of time  substr appears in value
+	 * @param value				input
+	 * @param substr			search string
+	 * @return number of times substring appears in value
+	 */
+	public static long countSubstr(final String value,final String substr){
+		return countSubstr(value, substr,true,false);
+	}
+	/**
+	 * Count the number of time substr appears in value
+	 * @param value				input
+	 * @param subStr			search string
+	 * @param caseSensitive		whether search should be case sensitive
+	 * @param allowOverlapping	whether to take overlapping into account
+	 * @return	count of times substring exist
+	 */
+	public static long countSubstr(final String value,final String subStr,final boolean caseSensitive,boolean allowOverlapping){
+		validate(value, NULL_STRING_PREDICATE, NULL_STRING_MSG_SUPPLIER);
+		return countSubstr(
+				caseSensitive?value:value.toUpperCase(), 
+				caseSensitive?subStr:subStr.toUpperCase(),
+				allowOverlapping, 
+				0
+		);
+	}
+	
+	/**
+	 * Count of times of substr apppears in value
+	 * @param value			input
+	 * @param substr		substr
+	 * @param allowOverlapping		whether to take overlapping into consideration
+	 * @param count	
+	 * @return
+	 */
+	private static long countSubstr(String value, String substr,boolean allowOverlapping,long count){
+		int position = value.indexOf(substr);
+		if (position == -1) {
+			return count;
+		}
+		int offset;
+		if (!allowOverlapping) {
+			offset = position + substr.length();
+		}else
+			offset = position +1;
+		return countSubstr(value.substring(offset), substr, allowOverlapping, ++count);
+		
+	}
+	/**
+	 * Validate input string whether is ended with search and case is sensitive
+	 * @param value 	the input string
+	 * @param search	the string to search
+	 * @return true of false
+	 */
+	public static boolean endsWith(final String value,final String search){
+		validate(value, NULL_STRING_PREDICATE, NULL_STRING_MSG_SUPPLIER);
+		return endsWith(value, search,value.length(),true);
+	}
+	/**
+	 * Validate whether the input string is ended with search String
+	 * @param value		 the input string
+	 * @param search	the search value
+	 * @param isCaseSensitive	true of false
+	 * @return true or false
+	 */
+	public  static boolean endsWith(final String value,final String search,boolean isCaseSensitive){
+		validate(value, NULL_STRING_PREDICATE, NULL_STRING_MSG_SUPPLIER);
+		return endsWith(value, search,value.length(),isCaseSensitive);
+	}
+	/**
+	 * Validate whether input value ends up with search
+	 * @param value		input string
+	 * @param search	string to search
+	 * @param position  position till which you want to search
+	 * @param caseSensitive	true or false
+	 * @return true or false
+	 */
+	public static boolean endsWith(final String value, final String search,final int position,final boolean caseSensitive){
+		validate(value, NULL_STRING_PREDICATE, NULL_STRING_MSG_SUPPLIER);
+		validate(value, NULL_STRING_PREDICATE, NULL_STRING_MSG_SUPPLIER);
+		int remainningLength = position - search.length();
+		if (caseSensitive) {
+			return value.indexOf(search,remainningLength) >-1;
+		}
+		return value.toUpperCase().indexOf(search.toUpperCase(),remainningLength) > -1;
+	}
+	
+	/**
+	 * Validate value is started with prefix value and case is not sensitive
+	 * @param value		the input string
+	 * @param prefix	the prefix value
+	 * @return	true of false
+	 */
+	public static boolean prefixWith(final String value,final String prefix){
+		return prefixWith(value, prefix,false);
+	}
+	/**
+	 * Validate the string value is started with prefix 
+	 * @param value			the input String
+	 * @param prefix		the prefix
+	 * @param caseSensitive	true or false
+	 * @return	true or false
+	 */
+	public static boolean prefixWith(final String value,final String prefix,final boolean caseSensitive){
+		validate(value, NULL_STRING_PREDICATE, NULL_STRING_MSG_SUPPLIER);
+		if (caseSensitive) {
+			return value.startsWith(prefix);
+		}
+		String _value = value.toLowerCase();
+		String _prefix = prefix.toLowerCase();
+		return _value.startsWith(_prefix);
+	}
 }
--- src/test/java/BUPT/ZLC/Strman/StrmanTest.java
+++ src/test/java/BUPT/ZLC/Strman/StrmanTest.java
@@ -11,6 +11,208 @@ import org.junit.Test;
 import static BUPT.ZLC.Strman.Strman.*;
 
 public class StrmanTest {
+	@Test
+	public void append_shouldAppendStringsToEndOfValue() throws Exception {
+		assertThat(append("s", "u","p","e","r","z","l","c"),equalTo("superzlc"));
+		assertThat(append("superzlc"),equalTo("superzlc"));
+		assertThat(append("", "superzlc"), equalTo("superzlc"));
+	}
 	
+	@Test (expected = IllegalArgumentException.class)
+	public void append_shouldThrowIllegalArgumentExceptionWhenValueIsNull() throws Exception{
+		append(null);
+	}
 	
+	@Test
+	public void appendArray_shouldAppendStringArrayToEndOfValue() throws Exception{
+		assertThat(appendArray("s", new String[]{"u","p","e","r","z","l","c"}),equalTo("superzlc"));
+		assertThat(appendArray("superzlc", new String[]{}), equalTo("superzlc"));
+		assertThat(appendArray("", new String[]{"superzlc"}), equalTo("superzlc"));
+	}
+	
+	@Test
+	public void at_shouldFindCharacterAtIndex() throws Exception {
+		assertThat(at("superzlc", 0), equalTo(Optional.of("s")));
+		assertThat(at("superzlc", 7), equalTo(Optional.of("c")));
+		assertThat(at("superzlc", -1), equalTo(Optional.of("c")));
+		assertThat(at("superzlc", -2), equalTo(Optional.of("l")));
+		assertThat(at("superzlc", 10), equalTo(Optional.empty()));
+		assertThat(at("superzlc", -10), equalTo(Optional.empty()));
+	}
+	
+	@Test
+	public void collapseWhitespace_shouldReplaceConsecutiveWhitespaceWithSingleSpace() throws Exception {
+		String[] fixture = {
+				"foo        bar",
+				"        foo      bar		",
+				" foo    bar		",
+				"    foo    bar "	
+		};
+		Arrays.stream(fixture).forEach(el->assertThat(collapseWhitespace(el),equalTo("foo bar")));
+	}
+	
+	@Test
+	public void between_shouldReturnArrayWIthStringsBetweenStartAndEnd() throws Exception{
+		assertThat(between("[Thanks][God]", "[", "]"), arrayContaining("Thanks","God"));
+		assertThat(between("<span>foolish</span>", "<span>", "</span>"), arrayContaining("foolish"));
+		assertThat(between("<span>foolish</span><span>man</span>", "<span>", "</span>"), arrayContaining("foolish","man"));
+	}
+	
+	@Test
+	public void between_shouldReturnFullStringWhenStartAndEndDoNotExist() throws Exception{
+		assertThat(between("[abc][def]", "<", ">"), arrayContaining("[abc][def]"));
+		assertThat(between("", "{", "}"), arrayContaining(""));
+	}
+	@Test (expected = IllegalArgumentException.class)
+	public void appendArray_shouldThrowIllegalArgumentExceptionWHenValueIsNull() throws Exception {
+		appendArray(null, new String[]{});
+	}
+	
+	@Test
+	public void chars_shouldReturnAllCharactersInString() throws Exception{
+		final String title = "title";
+		assertThat(chars(title), equalTo(new String[]{"t","i","t","l","e"}));
+	}
+	
+	@Test
+	public void collapseWhitespace_shouldReplaceConsecutiveWhitespaceBetweenMultipleStrings() throws Exception {
+		String input = " foo    bar     bazz   hello    world   ";
+		assertThat(collapseWhitespace(input), equalTo("foo bar bazz hello world"));
+	}
+	
+	@Test
+	public void contains_shouldReturnTrueWhenStringContainsNeedle() throws Exception {
+		String[] fixture = {
+				"foo bar",
+				"bar foo",
+				"foobar",
+				"foo"
+		};
+		Arrays.stream(fixture).forEach(el -> assertTrue(contains(el, "FOO")));
+	}
+	
+	@Test
+	public void containsWithCaseSensitiveTrue_shouldReturnTrueWhenStringContainsNeedle() throws Exception {
+		String[] fixture = {
+				"foo bar",
+				"bar foo",
+				"foo",
+				"foobar"
+		};
+		Arrays.stream(fixture).forEach(el -> assertFalse(contains(el, "Foo",true)));
+	}
+	
+	@Test
+	public void containsAll_shouldReturnTrueOnlyWhenAllNeedlesAreContainedInValue() throws Exception {
+		String[] fixture = {
+				"foo bar",
+				"bar foo",
+				"foobar"
+		};
+		Arrays.stream(fixture).forEach(el->assertTrue(containsAll(el,new String[]{"foo","bar"})));
+	}
+	
+	@Test
+	public void containsAny_shouldReturnTrueWhenAnyOfSearchNeedleExistInInputValue() throws Exception {
+		String[] fixture = {
+				"foo bar",
+				"bar foo",
+				"foobar"
+		};
+		Arrays.stream(fixture).forEach(el -> assertTrue(containsAny(el, new String[]{"foo", "bar", "test"})));
+	}
+	
+	public void containsAny_shouldReturnFalseWhenNoneOfSearchNeedleExistInInputValue() throws Exception{
+		String[] fixture = {
+				"foo bar",
+				"bar foo",
+				"foobar"
+		};
+		Arrays.stream(fixture).forEach(el->assertFalse(containsAny(el, new String[]{"FOO","BAR","Test"},true)));
+	}
+	
+	@Test
+	public void countSubstr_shouldCountCaseInsensitiveWithoutOverlapingInValue(){
+		assertThat(countSubstr("aaaAAAaaa", "aaa",false,false), equalTo(3L));
+	}
+	
+	@Test
+	public void countSubstr_shouldCountCaseSensitiveWithoutOverlappingInValue() {
+		assertThat(countSubstr("aaaAAAaaa", "aaa",true,false), equalTo(2L));
+	}
+	
+	@Test
+	public void countSubstr_shouldCountCaseInsensitiveWithOverlappingInValue() throws Exception {
+		assertThat(countSubstr("aaaAAAaaa", "aaa",false,true), equalTo(7L));
+	}
+	
+	@Test
+	public void countSubstr_shouldÇountSubStrCountCaseSensitiveWithOverlapInValue() {
+		assertThat(countSubstr("aaaAAAaaa", "AAA",true,true), equalTo(1L));
+	}
+	
+	@Test
+	public void endsWith_caseSensitive_ShouldBeTrueWhenStringEndsWithSearchStr() throws Exception{
+		String[] fixture = {
+				"foo bar",
+				"bar"
+		};
+		Arrays.stream(fixture).forEach(el->assertTrue(endsWith(el,"bar")));
+	}
+	
+	@Test
+	public void endsWith_notCaseSensitive_ShouldBeTrueWhenStringEndsWithSearchStr() throws Exception{
+		String[] fixture = {
+				"foo bar",
+				"bar"
+		};
+		
+		Arrays.stream(fixture).forEach(el->assertTrue(endsWith(el,"BAR",false)));
+	}
+	
+	@Test
+	public void endsWith_caseSensitiveAtPosition_ShouldBeTrueWhenSTringEndsWithSearchStr() throws Exception{
+		String[] fixtrue = {
+				"foo barr",
+				"barr"
+		};
+		Arrays.stream(fixtrue).forEach(
+				el->
+				assertTrue(
+						endsWith(el,"bar",el.length()-1,true)
+						
+						));
+	}
+	
+	@Test
+	public void endsWith_notCaseSensitiveAtPostion_ShouldBeTrueWhenStringEndsWithSearchStr() throws Exception{
+		String[] fixture={
+				"foo barr",
+				"barr"
+		};
+		Arrays.stream(fixture)
+		.forEach(
+				el->assertTrue(
+						endsWith(el,"BAR",el.length()-1,false)));
+	}
+	
+	@Test
+	public void prefixWithNotCaseSensitive_shouldReturnTrue(){
+		String[] fixture = {
+				"foobar",
+				"foo"
+		};
+		Arrays.stream(fixture).forEach(el->
+		assertTrue(prefixWith(el, "FOO")));
+	}
+	
+	@Test
+	public void prefixWithCaseSensitive_shouldReturnTrue(){
+		String[] fixture = {
+				"foobar",
+				"foo"
+		};
+		Arrays.stream(fixture).forEach(el->
+		assertTrue(prefixWith(el, "foo",true)));
+	}
 }
